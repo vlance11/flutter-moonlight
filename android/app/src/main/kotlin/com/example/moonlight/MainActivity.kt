2733b0fb6c65a8a975c0fbd94f0425fe9251e14c
Bhargav <bhargav.raviya@gmail.com>
2022-04-03 12:13:58 +0530

    Project Initial Setup

---
+++ android/app/src/main/kotlin/com/example/moonlight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moonlight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Moonlight")),
+      body: Center(
+        child: Text("Welcome to Flutter Moonlight Framework"),
+      ),
+    );
+  }
+}
